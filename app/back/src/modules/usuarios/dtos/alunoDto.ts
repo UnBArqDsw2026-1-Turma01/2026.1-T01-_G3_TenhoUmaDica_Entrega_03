173da14e9641f125e0d8c613fb4c7a691a2932bc
Diih <diogocorjesu@gmail.com>
2026-05-20 17:04:52 -0300

    feat: DTOs de Aluno e Admnistrador

---
+++ app/back/src/modules/usuarios/dtos/alunoDto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsNumber, MinLength } from 'class-validator';
+
+export class CriarAlunoDto {
+  @IsNotEmpty()
+  nome!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @MinLength(6)
+  senha!: string;
+
+  @MinLength(9)
+  matricula!: string;
+
+  @IsNumber()
+  semestreIngressante!: number;
+}
