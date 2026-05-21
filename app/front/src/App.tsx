76b01f381da1ba161ddf3b4c87902b3440b8ed45
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-05-20 23:52:19 -0300

    feat: Adiciona rota para a página de recuperação de senha

--- app/front/src/App.tsx
+++ app/front/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       {/* <Route path="/home" element={<Home />} /> */}
       <Route path="*" element={<div>Page Not Found</div>} />
     </Routes>
