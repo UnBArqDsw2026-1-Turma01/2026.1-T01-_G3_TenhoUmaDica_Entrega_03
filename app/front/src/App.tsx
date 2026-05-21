ec3feb3cbe3a5a6eca8e728bcde1d0b37692952b
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-05-20 23:36:32 -0300

    feat: Ativa a rota de cadastro no app

--- app/front/src/App.tsx
+++ app/front/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/register" element={<Register />} /> */}
+      <Route path="/register" element={<Register />} />
       {/* <Route path="/home" element={<Home />} /> */}
       <Route path="*" element={<div>Page Not Found</div>} />
     </Routes>
