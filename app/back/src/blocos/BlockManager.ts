f9895f4185e8e9db768a43bbb8dd8c6fce31a62d
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-05-22 23:19:43 -0300

    feat: utiliza Enum para tipos de texto em BlocoTexto no BlockManager

--- app/back/src/blocos/BlockManager.ts
+++ app/back/src/blocos/BlockManager.ts
@@ -1,6 +1,6 @@
 // O Cliente 
 
-import { BlocoTexto } from './BlocoTexto';
+import { BlocoTexto, TipoTexto } from './BlocoTexto';
 import { BlocoCodigo } from './BlocoCodigo';
 import { BlocoEquacao } from './BlocoEquacao';
 import { BlocoLista } from './BlocoLista';
@@ -9,11 +9,13 @@ class BlockManager {
     public static main(): void {
         console.log("Fórum #TenhoUmaDica: Renderizando Postagem");
 
-        const titulo = new BlocoTexto("Como usar Padrões de Projeto", "titulo 1");
-        const introducao = new BlocoTexto("Abaixo temos um exemplo de código e uma equação de complexidade.", "normal");
+        // Instanciando folhas genéricas, também chamando os tipos de texto definidos com o Enum
+        const titulo = new BlocoTexto("Como usar Padrões de Projeto", TipoTexto.TITULO_1);
+        const introducao = new BlocoTexto("Abaixo temos um exemplo de código e uma equação de complexidade.", TipoTexto.NORMAL);
         const codigo = new BlocoCodigo("console.log('Padrão Composite rodando!');", "typescript");
         const equacao = new BlocoEquacao("O(n) = n^2");
 
+        // Instanciando o Composite
         const postagemCompleta = new BlocoLista();
         postagemCompleta.adicionarBloco(titulo);
         postagemCompleta.adicionarBloco(introducao);
@@ -21,6 +23,7 @@ class BlockManager {
         postagemCompleta.adicionarBloco(equacao);
         const resultadoHTML = postagemCompleta.exibir();
         
+        // Chamada uniforme
         console.log("\nResultado final do Markdown:");
         console.log(resultadoHTML);
     }
