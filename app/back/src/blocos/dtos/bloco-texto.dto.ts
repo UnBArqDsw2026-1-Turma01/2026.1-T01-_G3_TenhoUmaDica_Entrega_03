2a2a1973b7c1829e8f9044344c870c52ac5db745
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-05-22 23:21:58 -0300

    feat: atualiza CriarBlocoTextoDto para utilizar Enum TipoTexto em vez de strings

--- app/back/src/blocos/dtos/bloco-texto.dto.ts
+++ app/back/src/blocos/dtos/bloco-texto.dto.ts
@@ -1,9 +1,12 @@
-import { IsIn, IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+import { TipoTexto } from '../BlocoTexto'; // Ajuste o caminho relativo se necessário
 
 export class CriarBlocoTextoDto {
   @IsNotEmpty()
   texto!: string;
 
-  @IsIn(['normal', 'titulo 1', 'citacao'])
-  tipo!: string;
+  @IsEnum(TipoTexto, {
+    message: 'O tipo deve ser um dos valores válidos do TipoTexto (ex: normal, titulo 1, citacao)'
+  })
+  tipo!: TipoTexto;
 }
