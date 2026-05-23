917d7481fcd5d712c09ce57238a93ed49349f24e
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-05-22 23:19:54 -0300

    feat: refatora BlocoTexto para utilizar Enum TipoTexto em vez de strings

--- app/back/src/blocos/BlocoTexto.ts
+++ app/back/src/blocos/BlocoTexto.ts
@@ -2,21 +2,35 @@
 
 import { ComponenteBloco } from './ComponenteBloco';
 
+export enum TipoTexto {
+    TITULO_1 = 'titulo 1',
+    TITULO_2 = 'titulo 2',
+    TITULO_3 = 'titulo 3',
+    CITACAO = 'citacao',
+    NORMAL = 'normal'
+}
+
 export class BlocoTexto implements ComponenteBloco {
     private texto: string;
-    private tipo: string;
+    private tipo: TipoTexto; 
 
-    constructor(texto: string, tipo: string) {
+    constructor(texto: string, tipo: TipoTexto) {
         this.texto = texto;
         this.tipo = tipo;
     }
 
     public exibir(): string {
-        if (this.tipo === 'titulo 1') {
+        
+        if (this.tipo === TipoTexto.TITULO_1) {
             return `# ${this.texto}`;
-        } else if (this.tipo === 'citacao') {
+        } else if (this.tipo === TipoTexto.TITULO_2) {
+            return `## ${this.texto}`;
+        } else if (this.tipo === TipoTexto.TITULO_3) {
+            return `### ${this.texto}`;
+        } else if (this.tipo === TipoTexto.CITACAO) {
             return `> ${this.texto}`;
         }
-        return this.texto;
+
+        return this.texto; 
     }
 }
